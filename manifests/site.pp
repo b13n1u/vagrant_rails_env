6b49b82fd568003aed92bed4f709a13550d9be9b
U-bieniu-pc\bieniu <bieniu@bieniu-pc.(none)>
2014-12-10 23:42:56 +0100

    added sqlite

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,8 +1,16 @@
+package {'sqlite3':
+   ensure => 'installed',
+}
+
+package {'libsqlite3-dev':
+   ensure => 'installed',
+   require => Package['sqlite3'],
+}
+
 package {'ruby-dev':
    ensure => 'installed',
 
 }
-
 package { 'rails':
    ensure => 'installed',
    provider => 'gem',
